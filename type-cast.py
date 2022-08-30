fb5dc6c3d3b322dc6a1be90a547e57e511b9a7a7
Do Truong Manh <manhcntt2.1@gmail.com>
2022-08-31 05:44:16 +0700

    typecasting

---
+++ type-cast.py
@@ -0,0 +1,13 @@
+# type casting: convert the datatype of a value to another datatype
+
+x = 1    # int
+y = 2.0  # float
+z = "3"  # string
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(y)
+print(z*3)
