c739a00181ff82206e02f01295614e8563d0ebe2
Do Truong Manh <manhcntt2.1@gmail.com>
2022-08-31 06:12:16 +0700

    math functions

---
+++ math-functions.py
@@ -0,0 +1,32 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(pi)
+
+# Round a number to a given precision in decimal digits.
+print((round(pi)))
+
+# round a number up
+print(math.ceil(pi))
+
+# round a number down
+print(math.floor(pi))
+
+# absolute
+print(abs(pi))
+
+# pow
+print(pow(pi, 2))
+
+# sqrt
+print(math.sqrt(pi))
+
+# max
+print(max(x,y,z))
+
+# min
+print(min(x,y,z))
