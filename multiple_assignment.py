c8fa724aaf4b60463b3b7fac511ddcb516c0d57c
Do Truong Manh <manhcntt2.1@gmail.com>
2022-08-31 04:29:15 +0700

    multiple asssignment in python

---
+++ multiple_assignment.py
@@ -0,0 +1,28 @@
+# multiple assignment: allow us to assign multiple variables at the same time in one line of code
+
+# name = "Do"
+# age = 25
+# height = 170.5
+# attractive = True
+
+# equivalent
+name, age, height, attractive = "Do", 25, 170.5, True
+print(name)
+print(age)
+print(height)
+print(attractive)
+#--------------------------------------------------------------
+# general cost of fruits in Vietnam (VND/kg)
+
+# apple = 80000
+# orange = 80000
+# banana = 80000
+# pineapple = 80000
+
+# equivalent
+apple = orange = banana = pineapple = 80000
+
+print(apple)
+print(orange)
+print(banana)
+print(pineapple)
