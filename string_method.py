62c5204337a5b8496abef333abece769d710ad2a
Do Truong Manh <manhcntt2.1@gmail.com>
2022-08-31 05:09:48 +0700

    some string method examples

---
+++ string_method.py
@@ -0,0 +1,34 @@
+name = 'Manh Do'
+
+# length of the string
+print(len(name))
+
+# find a substring in a string
+# index is first index which has finded
+print(name.find("M"))
+
+# capitalize
+print(name.capitalize())
+
+# upper
+print(name.upper())
+
+# lower
+print(name.lower())
+
+# isdigit
+print(name.isdigit())
+
+# isalpha
+# space not alphabet
+print(name.isalpha())
+
+# count
+# substring non-overlapping
+print(name.count('o'))
+
+# replace
+print(name.replace('a', 'o', 1))
+
+# multiple times of string
+print(name*3)
