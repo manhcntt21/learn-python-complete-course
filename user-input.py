e798c676711e6e3dc3b6a95885955c18da60be77
Do Truong Manh <manhcntt2.1@gmail.com>
2022-08-31 06:00:41 +0700

    user input

---
+++ user-input.py
@@ -0,0 +1,10 @@
+# input receive values that user type from console
+# and validate those values
+
+name = input("what is your name?: ")
+age = int(input("how old are you?: "))
+height = float(input("how tall are you?: "))
+
+print("Hello " + name)
+print("you are " + str(age) + " years old")
+print("you are " + str(height) + "cm tall")
