304136bde5c933cd9ced9197aacad6193b0a0822
Do Truong Manh <manhcntt2.1@gmail.com>
2022-09-01 00:55:24 +0700

    a 2d-list

---
+++ 2d-list.py
@@ -0,0 +1,14 @@
+# 2d-lists: a list of lists
+
+drinks = ["coffe", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hot-dog"]
+dessert = ["cake", "ice cream"]
+
+# a 2d-list
+food = [drinks, dinner, dessert]
+print(food)
+
+# indexing
+print(food[0])
+print(food[0][0])
+print(food[0][1])
