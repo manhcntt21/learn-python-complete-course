0f7b7f1f68475d8fa17ef4a04b152c151f17ec5a
Do Truong Manh <manhcntt2.1@gmail.com>
2022-09-03 14:28:20 +0700

    module python

---
+++ modules.py
@@ -0,0 +1,30 @@
+# module: a file containing python. May contain functions, classes, etc.
+# used with modular programming, which is to separate a program into parts
+
+# import messages
+# import messages as m
+# from messages import hello, bye
+# from messages import * (this one is dangerous)
+
+# help("modules")
+
+# -------------------------------------------------------------------
+# import messages
+#
+# messages.hello()
+# messages.bye()
+
+
+# -------------------------------------------------------------------
+# import messages as msg
+#
+# msg.hello()
+# msg.bye()
+
+# -------------------------------------------------------------------
+from messages import hello, bye
+
+hello()
+bye()
+# -------------------------------------------------------------------
+
